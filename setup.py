feb3d60a8fff9c41812838499f8b01d2a4107de1
lewisgmorris <lewis.morris@gmail.com>
2020-05-05 08:46:14 +0100

    init

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='mapextrackt',
     version='0.01',
@@ -8,5 +15,7 @@ setup(
     license='MIT',
     author='Lewis Morris',
     author_email='lewis.morris@gmail.com',
-    description='Pytorch Feature Map Extractor'
+    description='Pytorch Feature Map Extractor',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
